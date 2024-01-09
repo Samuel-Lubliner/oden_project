ce07c25dbff87216362d11c79c5fd2bc744178d0
Samuel Lubliner <samlubliner@gmail.com>
2024-01-09 00:27:01 -0600

    add css

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,9 +1,16 @@
+import _ from 'lodash';
 import myName from './myName';
+import './style.css';
 
 function component() {
     const element = document.createElement('div');
+
+    // element.textContent = myName('Sam');
+    
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   
-    element.textContent = myName('Sam');
+    element.classList.add('hello');
+    
     return element;
   }
   
