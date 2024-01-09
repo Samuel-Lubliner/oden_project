85da5a1622be161d6639ae51d989d1d1225c43f6
Samuel Lubliner <samlubliner@gmail.com>
2024-01-09 01:28:35 -0600

    add icon

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,17 +1,21 @@
 import _ from 'lodash';
-import myName from './myName';
 import './style.css';
+import Icon from './icon.png';
 
 function component() {
     const element = document.createElement('div');
 
-    // element.textContent = myName('Sam');
-    
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
-    element.classList.add('hello');
-    
-    return element;
-  }
-  
-  document.body.appendChild(component());
+// Lodash, now imported by this script
+element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+element.classList.add('hello');
+
+// Add the image to our existing div.
+const myIcon = new Image();
+myIcon.src = Icon;
+
+element.appendChild(myIcon);
+
+return element;
+}
+
+document.body.appendChild(component());
